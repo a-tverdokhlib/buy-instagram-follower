882d07337a2452c8b9e40ba4705c13adc1cf707e
tverdokhlibt <tverdokhlibt@gmail.com>
2022-02-13 18:04:00 +0200

    Swiper Custom

--- src/components/atoms/MySwipper.tsx
+++ src/components/atoms/MySwipper.tsx
@@ -1,34 +1,134 @@
 type Props = {
   readonly children: ReadonlyArray<Object>
+  readonly prevRef: any
+  readonly nextRef: any
+  readonly activeIndexChange: any
+  readonly swiperReachStart: any
+  readonly swiperReachEnd: any
 }
-type children = readonly Object[]
 
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import 'swiper/css/scrollbar'
 
+import React, { ForwardRefRenderFunction, useEffect, useState } from 'react'
 import { A11y, Navigation, Pagination, Scrollbar } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
 
-export const MySwipper: React.FC<Props> = ({ children }) => (
-  <Swiper
-    className="flex w-full"
-    modules={[Navigation, Pagination, Scrollbar, A11y]}
-    spaceBetween={10}
-    slidesPerView={3}
-    navigation
-    pagination={{ clickable: true }}
-    scrollbar={{ draggable: true }}
-    onSwiper={(swiper) => console.log(swiper)}
-    onSlideChange={() => console.log('slide change')}
-  >
-    {children
-      ? children.map((slideContent, index) => (
-          <SwiperSlide key={index} virtualIndex={index}>
-            <div className="flex px-10 justify-center">{slideContent}</div>
-          </SwiperSlide>
-        ))
-      : null}{' '}
-  </Swiper>
-)
+const MyComponentRenderFn: ForwardRefRenderFunction<any, Props> = (
+  {
+    children,
+    prevRef,
+    nextRef,
+    activeIndexChange,
+    swiperReachStart,
+    swiperReachEnd,
+  },
+  ref,
+) => {
+  const [activeIndex, setActiveIndex] = useState(0)
+  useEffect(() => {
+    const swiper = document.querySelector('.swiper')?.swiper
+    setActiveIndex(swiper.activeIndex)
+  }, [])
+
+  useEffect(() => {
+    if (activeIndex === 0) swiperReachStart()
+    const swiper = document.querySelector('.swiper')?.swiper
+    if (swiper.isEnd) swiperReachEnd(1)
+    else swiperReachEnd(0)
+    if (swiper.isBeginning) swiperReachStart(1)
+    else swiperReachStart(0)
+  }, [activeIndex])
+
+  const nextSlide = () => {
+    const swiper = document.querySelector('.swiper')?.swiper
+    swiper.slideNext()
+  }
+  const prevSlide = () => {
+    const swiper = document.querySelector('.swiper')?.swiper
+    swiper.slidePrev()
+  }
+
+  return (
+    <Swiper
+      className="flex w-full"
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={10}
+      breakpoints={{
+        // when window width is >= 640px
+        640: {
+          width: 640,
+          slidesPerView: 1,
+        },
+        // when window width is >= 768px
+        768: {
+          width: 768,
+          slidesPerView: 2,
+        },
+        // when window width is >= 768px
+        1024: {
+          width: 1024,
+          slidesPerView: 3,
+        },
+        1280: {
+          width: 1280,
+          slidesPerView: 4,
+        },
+      }}
+      //   navigation={{
+      //     prevEl: prevRef.current ? prevRef.current : undefined,
+      //     nextEl: nextRef.current ? nextRef.current : undefined,
+      //   }}
+      pagination={{ clickable: true }}
+      // scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={(e) => setActiveIndex(e.activeIndex)}
+      onReachBeginning={swiperReachStart}
+      onReachEnd={swiperReachEnd}
+    >
+      {children
+        ? children.map((slideContent, index) => (
+            <SwiperSlide key={index} virtualIndex={index}>
+              <div className="flex px-10 justify-center">{slideContent}</div>
+            </SwiperSlide>
+          ))
+        : null}{' '}
+      <div data-toggle="prev" ref={prevRef} onClick={(e) => prevSlide()}></div>
+      <div data-toggle="next" ref={nextRef} onClick={(e) => nextSlide()}></div>
+    </Swiper>
+  )
+}
+
+export const MySwipper = React.forwardRef(MyComponentRenderFn)
+
+// export const MySwipper: React.FC<Props> = ({ children }) => {
+//   const nextSlide = () => {
+//     console.log('Next Slide')
+//   }
+//   const prevSlide = () => {
+//     console.log('Prev Slide')
+//   }
+//   return (
+//     <Swiper
+//       className="flex w-full"
+//       modules={[Navigation, Pagination, Scrollbar, A11y]}
+//       spaceBetween={10}
+//       slidesPerView={3}
+//       navigation
+//       pagination={{ clickable: true }}
+//       // scrollbar={{ draggable: true }}
+//       onSwiper={(swiper) => console.log(swiper)}
+//       onSlideChange={(e) => console.log('slide change => ', e)}
+//     >
+//       {children
+//         ? children.map((slideContent, index) => (
+//             <SwiperSlide key={index} virtualIndex={index}>
+//               <div className="flex px-10 justify-center">{slideContent}</div>
+//             </SwiperSlide>
+//           ))
+//         : null}{' '}
+//     </Swiper>
+//   )
+// }
--- src/components/organisms/Products.tsx
+++ src/components/organisms/Products.tsx
@@ -1,14 +1,37 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import React from 'react'
 
 import { Card } from '@/components/atoms/Card'
 import { MySwipper } from '@/components/atoms/MySwipper'
 
 export const Products: React.VFC = () => {
+  const swiperRef = useRef<any>(null)
+  const prevRef = useRef<any>(null)
+  const nextRef = useRef<any>(null)
+  // const [swiperCurrentIndex, setSwiperCurrentIndex] = useState(0)
+  const [swiperReachStarted, setSwiperReachStarted] = useState(0)
+  const [swiperReachEnded, setSwiperReachEnded] = useState(0)
+  const prevClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    prevRef.current?.click()
+  }
+  const nextClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    nextRef.current?.click()
+  }
+
+  const swiperIndexChange = (e: any) => {}
+
+  const swiperReachStart = (e: any) => {
+    setSwiperReachStarted(e)
+  }
+
+  const swiperReachEnd = (e: any) => {
+    setSwiperReachEnded(e)
+  }
   return (
-    <div className="flex flex-col items-center flex-wrap bg-[#14141F]">
-      <div className="flex w-full min-w-[500] space-x-3 p-12 justify-left">
-        <div className="flex overflow-hidden rounded-full items-center justify-center w-10 h-10">
-          <span className="animate-ping-slow z-[2] inline-flex h-6 w-6 rounded-full bg-red-700 border-2 border-red-700 opacity-75"></span>
+    <div className="flex flex-col items-center flex-wrap min-h-screen bg-[#030304]">
+      <div className="flex w-full min-w-[500] p-12 justify-left">
+        <div className="flex overflow-hidden rounded-full items-center justify-center w-10">
+          <span className="animate-ping-slow z-[2] inline-flex h-6 w-6 rounded-full bg-red-600 border-2 border-red-600 opacity-75"></span>
           <div className="absolute z-[3] rounded-full bg-[#14141F]">
             <svg
               className="h-8 w-8 text-red-600 m-auto"
@@ -28,13 +51,70 @@ export const Products: React.VFC = () => {
             </svg>
           </div>
         </div>
-        <div className="flex">
-          <h1 className="text-4xl text-white font-semibold md:text-5xl md:font-bold">
+        <div className="flex items-center justify-center">
+          <h1 className="text-3xl text-white font-semibold md:text-5xl md:font-bold">
             Products
           </h1>
         </div>
+        <div className="flex space-x-2 ml-auto">
+          <div
+            className={
+              swiperReachStarted === 0
+                ? 'flex cursor-pointer w-10 h-10 md:w-14 md:h-14 justify-center items-center bg-gradient-to-br from-violet-800 to-fuchsia-500 rounded-full'
+                : 'flex cursor-pointer w-10 h-10 md:w-14 md:h-14 justify-center items-center bg-gray-700 rounded-full'
+            }
+            onClick={(e) => prevClick(e)}
+          >
+            <svg
+              className="h-8 w-8 text-white"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              {' '}
+              <line x1="19" y1="12" x2="5" y2="12" />{' '}
+              <polyline points="12 19 5 12 12 5" />
+            </svg>
+          </div>
+          <div
+            className={
+              swiperReachEnded === 0
+                ? 'flex cursor-pointer w-10 h-10 md:w-14 md:h-14 justify-center items-center bg-gradient-to-br from-violet-800 to-fuchsia-500 rounded-full'
+                : 'flex cursor-pointer w-10 h-10 md:w-14 md:h-14 justify-center items-center bg-gray-700 rounded-full'
+            }
+            onClick={(e) => nextClick(e)}
+          >
+            <svg
+              className="h-8 w-8 text-white"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              strokeWidth="2"
+              stroke="currentColor"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              {' '}
+              <path stroke="none" d="M0 0h24v24H0z" />{' '}
+              <line x1="5" y1="12" x2="19" y2="12" />{' '}
+              <line x1="13" y1="18" x2="19" y2="12" />{' '}
+              <line x1="13" y1="6" x2="19" y2="12" />
+            </svg>
+          </div>
+        </div>
       </div>
-      <MySwipper>
+      <MySwipper
+        ref={swiperRef}
+        prevRef={prevRef}
+        nextRef={nextRef}
+        activeIndexChange={swiperIndexChange}
+        swiperReachStart={swiperReachStart}
+        swiperReachEnd={swiperReachEnd}
+      >
         <div className="overflow-hidden flex flex-wrap w-full min-w-[250px] max-w-xs bg-[#343444] rounded-[25px] justify-center hover:shadow-[0_1px_10px_5px_rgba(0,0,0,0.5)]">
           <div className="w-full mt-3 p-5">
             <svg
