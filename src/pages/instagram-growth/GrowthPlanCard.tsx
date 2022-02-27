6fbe037f48360dbe36f90138d1b0220a1266cdc3
tverdokhlibt <tverdokhlibt@gmail.com>
2022-02-27 09:35:44 +0200

    Global Font Updates

--- src/pages/instagram-growth/GrowthPlanCard.tsx
+++ src/pages/instagram-growth/GrowthPlanCard.tsx
@@ -72,7 +72,7 @@ export const GrowthPlanCard: React.VFC<Props> = (props) => {
         </div>
       </div>
       {/* background-image: linear-gradient(to right,#ff7802 0%,#fea921 100%),linear-gradient(to right,#ff7802 0%,#fea921 100%); */}
-      <div className="w-full flex text-sm h-9 text-gray-300 space-x-1 border-b-[1px] border-gray-600 items-center justify-center text-center">
+      <div className="w-full flex text-sm h-8 text-gray-300 space-x-1 border-b-[1px] border-gray-600 items-center justify-center text-center">
         <svg
           className="h-4 w-4 text-[#DC39FC]"
           fill="none"
@@ -86,9 +86,11 @@ export const GrowthPlanCard: React.VFC<Props> = (props) => {
             d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
           />
         </svg>
-        <span>{props.followerDesc}</span>
+        <span>
+          <span className="text-[13px] ls:text-sm">{props.followerDesc}</span>
+        </span>
       </div>
-      <div className="w-full flex text-sm h-9 text-gray-300 space-x-1 border-b-[1px] border-gray-600 items-center justify-center text-center">
+      <div className="w-full flex text-sm h-8 text-gray-300 space-x-1 border-b-[1px] border-gray-600 items-center justify-center text-center">
         <svg
           className="h-3 w-3 text-[#DC39FC]"
           viewBox="0 0 24 24"
@@ -103,10 +105,12 @@ export const GrowthPlanCard: React.VFC<Props> = (props) => {
           <path d="M7 11V7a5 5 0 0 1 9.9-1" />
         </svg>
         <span>
-          <span className="font-semibold text-gray-400">{props.viewDesc}</span>{' '}
+          <span className="font-semibold text-gray-400 text-[13px] ls:text-sm">
+            {props.viewDesc}
+          </span>{' '}
         </span>
       </div>
-      <div className="w-full flex text-sm h-9 text-gray-300 space-x-1 border-b-[1px] border-gray-600 items-center justify-center text-center">
+      <div className="w-full flex text-sm h-8 text-gray-300 space-x-1 border-b-[1px] border-gray-600 items-center justify-center text-center">
         <svg
           className="h-3 w-3 text-[#DC39FC]"
           viewBox="0 0 24 24"
@@ -120,10 +124,12 @@ export const GrowthPlanCard: React.VFC<Props> = (props) => {
           <path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3" />
         </svg>
         <span>
-          <span className="font-semibold">{props.likeDesc}</span>
+          <span className="font-semibold text-[13px] ls:text-sm">
+            {props.likeDesc}
+          </span>
         </span>
       </div>
-      <div className="w-full flex text-sm h-9 text-gray-300 space-x-1 border-b-[1px] border-gray-600 items-center justify-center text-center">
+      <div className="w-full flex text-sm h-8 text-gray-300 space-x-1 border-b-[1px] border-gray-600 items-center justify-center text-center">
         <svg
           className="h-3 w-3 text-[#DC39FC]"
           viewBox="0 0 24 24"
@@ -137,12 +143,12 @@ export const GrowthPlanCard: React.VFC<Props> = (props) => {
           <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
         </svg>
         <span>
-          <span className="font-semibold text-gray-400">
+          <span className="font-semibold text-gray-400 text-[13px] ls:text-sm">
             {props.autoSaveDesc}
           </span>
         </span>
       </div>
-      <div className="w-full flex text-sm h-9 text-gray-300 space-x-1 border-b-[1px] border-gray-600 items-center justify-center text-center">
+      <div className="w-full flex text-sm h-8 text-gray-300 space-x-1 border-b-[1px] border-gray-600 items-center justify-center text-center">
         <svg
           className="h-4 w-4 text-[#DC39FC]"
           width="24"
@@ -162,12 +168,12 @@ export const GrowthPlanCard: React.VFC<Props> = (props) => {
           <line x1="17" y1="4" x2="19.75" y2="6" />
         </svg>
         <span>
-          <span className="font-semibold text-gray-400">
+          <span className="font-semibold text-gray-400 text-[13px] ls:text-sm">
             {props.autoCommentDesc}
           </span>{' '}
         </span>
       </div>
-      <div className="w-full flex text-sm h-9 text-gray-300 space-x-1 border-b-[1px] border-gray-600 items-center justify-center text-center">
+      <div className="w-full flex text-sm h-8 text-gray-300 space-x-1 border-b-[1px] border-gray-600 items-center justify-center text-center">
         <svg
           className="h-3 w-3 text-[#DC39FC]"
           viewBox="0 0 24 24"
@@ -181,12 +187,12 @@ export const GrowthPlanCard: React.VFC<Props> = (props) => {
           <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
         </svg>
         <span>
-          <span className="font-semibold text-gray-400">
+          <span className="font-semibold text-gray-400 text-[13px] ls:text-sm">
             {props.autoImpressDesc}
           </span>
         </span>
       </div>
-      <div className="w-full flex text-sm h-9 text-gray-300 space-x-1 border-b-[1px] border-gray-600 items-center justify-center text-center">
+      <div className="w-full flex text-sm h-8 text-gray-300 space-x-1 border-b-[1px] border-gray-600 items-center justify-center text-center">
         <svg
           className="h-4 w-4 text-[green]"
           width="24"
@@ -201,9 +207,11 @@ export const GrowthPlanCard: React.VFC<Props> = (props) => {
           {' '}
           <path stroke="none" d="M0 0h24v24H0z" /> <path d="M5 12l5 5l10 -10" />
         </svg>
-        <span className="text-[green]">{props.profileVisitDesc}</span>
+        <span className="text-[green] text-[13px] ls:text-sm">
+          {props.profileVisitDesc}
+        </span>
       </div>
-      <div className="w-full flex text-sm h-9 text-gray-300 space-x-1 border-b-[1px] border-gray-600 items-center justify-center text-center">
+      <div className="w-full flex text-sm h-8 text-gray-300 space-x-1 border-b-[1px] border-gray-600 items-center justify-center text-center">
         <svg
           className="h-4 w-4 text-[green]"
           width="24"
@@ -218,9 +226,11 @@ export const GrowthPlanCard: React.VFC<Props> = (props) => {
           {' '}
           <path stroke="none" d="M0 0h24v24H0z" /> <path d="M5 12l5 5l10 -10" />
         </svg>
-        <span className="text-[green]">{props.futurePostDesc}</span>
+        <span className="text-[green] text-[13px] ls:text-sm">
+          {props.futurePostDesc}
+        </span>
       </div>
-      <div className="w-full flex text-sm h-9 text-gray-300 space-x-1 border-b-[1px] border-gray-600 items-center justify-center text-center">
+      <div className="w-full flex text-sm h-8 text-gray-300 space-x-1 border-b-[1px] border-gray-600 items-center justify-center text-center">
         <svg
           className="h-4 w-4 text-[green]"
           width="24"
@@ -235,11 +245,13 @@ export const GrowthPlanCard: React.VFC<Props> = (props) => {
           {' '}
           <path stroke="none" d="M0 0h24v24H0z" /> <path d="M5 12l5 5l10 -10" />
         </svg>
-        <span className="text-[green]">{props.oneTimePaymentDesc}</span>
+        <span className="text-[green] text-[13px] ls:text-sm">
+          {props.oneTimePaymentDesc}
+        </span>
       </div>
       <div
         onClick={props.onPurchaseClicked}
-        className="w-full flex text-sm h-9 text-gray-300 space-x-1  items-center justify-center text-center cursor-pointer hover:scale-110 transition-all duration-500"
+        className="w-full flex text-sm h-8 text-gray-300 space-x-1  items-center justify-center text-center cursor-pointer hover:scale-110 transition-all duration-500"
       >
         <svg
           className="h-5 w-5 text-yellow-400"
