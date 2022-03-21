c2d654a48ccdfeb9759e8c402fcbff5bd576eea7
tverdokhlibt <tverdokhlibt@gmail.com>
2022-03-21 19:08:25 +0200

    Minor updates

--- src/components/atoms/SmallLoading.tsx
+++ src/components/atoms/SmallLoading.tsx
@@ -2,7 +2,7 @@ export const SmallLoading: React.VFC = () => {
   return (
     <div className="flex w-10 h-10 items-center justify-center">
       <svg
-        className="absolute rotating h-10 w-10 text-red-600"
+        className="absolute rotating h-9 w-9 text-red-600"
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
@@ -15,7 +15,7 @@ export const SmallLoading: React.VFC = () => {
         <path d="M16.24 7.76a6 6 0 0 1 0 8.49m-8.48-.01a6 6 0 0 1 0-8.49m11.31-2.82" />
       </svg>
       <svg
-        className="absolute reverse-rotating h-10 w-10 text-red-600"
+        className="absolute reverse-rotating h-14 w-14 text-red-600"
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
