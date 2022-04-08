38aa877791f08984e92b6e1a45be7c634ca7a5ee
tverdokhlibt <tverdokhlibt@gmail.com>
2022-04-08 18:50:41 +0300

    4/8 updates

--- src/pages/api/instagram/index.ts
+++ src/pages/api/instagram/index.ts
@@ -15,201 +15,227 @@ export default connectDB(
   }),
 )
 
-async function getUserInfo(req, res) {
-  const { username } = req.query
-  if (username === '') {
-    throw 'No username given'
+async function firstWay(username, host, port, auth, callback) {
+  const agent = tunnel.httpsOverHttp({
+    proxy: {
+      host: host,
+      port: port,
+      proxyAuth: auth,
+    },
+  })
+
+  const responsePromise = await axios.request({
+    url: `https://www.instagram.com/${username}/?__a=1`,
+    method: 'get',
+    headers: {
+      'User-Agent': 'Chrome',
+    },
+    httpsAgent: agent,
+  })
+
+  const respData = responsePromise.data
+  if (respData.graphql !== undefined) {
+    return respData.graphql
+  } else {
+    return null
   }
+}
 
-  // ********* Attempted **********
-  // let httpsProxyAgent = require('https-proxy-agent')
-  // var agent = new httpsProxyAgent(
-  //   'http://sanjananb:sanjananb@s1.airproxy.io:31005',
-  // )
-
-  // var config = {
-  //   url: `https://www.instagram.com/${username}/?__a=1`,
-  //   httpsAgent: agent,
-  // }
-
-  // axios
-  //   .request(config)
-  //   .then((result) => {
-  //     return res.status(200).json({
-  //       data: result,
-  //     })
-  //   })
-  //   .catch((err) => {
-  //     console.log(err)
-  //     return res.status(200).json({
-  //       data: {},
-  //       error: err,
-  //     })
-  //   })
-
-  // ********** Attempted ***********
-  // axios
-  //   .get(`https://www.instagram.com/${username}/?__a=1`, {
-  //     proxy: {
-  //       host: 's1.airproxy.io',
-  //       port: 31005,
-  //       auth: { username: 'sanjananb', password: 'sanjananb' },
-  //     },
-  //   })
-  //   .then((result) => {
-  //     return res.status(200).json({
-  //       data: result,
-  //     })
-  //   })
-  //   .catch((err) => {
-  //     return res.status(200).json({
-  //       data: {},
-  //       error: err,
-  //     })
-  //   })
-
-  // ********** Attempted *********
-  // const url = `https://www.instagram.com/${username}/?__a=1/sanjananb:sanjananb@s2.airproxy.io:31005`
-
-  // fetch(url)
-  //   .then((response) => response.text())
-  //   .then((response) => {
-  //     return res.status(200).json({
-  //       data: response,
-  //     })
-  //   })
-  //   .catch((error) => {
-  //     return res.status(501).json({
-  //       data: {},
-  //     })
-  //   })
-
-  // ********** Attempted *********
-  // const HttpsProxyAgent = require('https-proxy-agent')
-
-  // const axiosDefaultConfig = {
-  //   baseURL: `https://www.instagram.com/${username}/?__a=1`,
-  //   proxy: false,
-  //   httpsAgent: new HttpsProxyAgent(
-  //     'http://sanjananb:sanjananb@s2.airproxy.io:31005',
-  //   ),
-  // }
-  // const axios = require('axios').create(axiosDefaultConfig)
-  // axios
-  //   .get('42')
-  //   .then(function (response) {
-  //     console.log(`Response with axios was ok: ${response.status}`)
-  //     console.log(response)
-  //     // return res.status(200).json({
-  //     //   data: response,
-  //     // })
-  //   })
-  //   .catch(function (error) {
-  //     console.log(error)
-  //     return res.status(200).json({
-  //       data: {},
-  //       error: error,
-  //     })
-  //   })
-
-  // 1st way
-  // const agent = tunnel.httpsOverHttp({
-  //   proxy: {
-  //     host: '138.68.76.104',
-  //     port: 8058,
-  //     proxyAuth: 'admin:p8FNPlwEEh0R',
-  //   },
-  // })
-
-  // const responsePromise = await axios.request({
-  //   url: `https://www.instagram.com/${username}/?__a=1`,
-  //   method: 'get',
-  //   headers: {
-  //     'User-Agent': 'Chrome',
-  //   },
-  //   httpsAgent: agent,
-  // })
-
-  // console.log('Response =>', responsePromise)
-  // const respData = responsePromise.data
-  // if (respData.graphql !== undefined) {
-  //   console.log('Success!!!')
-  //   return res.status(200).json({
-  //     data: responsePromise.data,
-  //   })
-  // }
-
-  //2nd way
+async function secondWay(username, host, port, auth, callback) {
   const axiosDefaultConfig = {
     baseURL: `https://www.instagram.com/${username}/?__a=1`,
     proxy: {
-      host: 's2.airproxy.io',
-      port: 31005,
+      host: host,
+      port: port,
       protocol: 'http',
-      auth: {
-        username: 'sanjananb',
-        password: 'sanjananb',
-      },
+      auth: auth,
     },
   }
 
   const axiosFixed = require('axios-https-proxy-fix').create(axiosDefaultConfig)
-  axiosFixed
+  await axiosFixed
     .get()
     .then(async function (response) {
       console.log(response)
       if (response.data) {
         if (response.data.graphql !== undefined) {
-          return res.status(200).json({
-            data: response.data.graphql,
-            status: 'success',
-          })
+          return response.data.graphql
         } else {
-          // return res.status(200).json({
-          //   data: {},
-          //   status: 'success',
-          // })
-          const agent = tunnel.httpsOverHttp({
-            proxy: {
-              host: '138.68.76.104',
-              port: 8058,
-              proxyAuth: 'admin:p8FNPlwEEh0R',
-            },
-          })
-
-          const responsePromise = await axios.request({
-            url: `https://www.instagram.com/${username}/?__a=1`,
-            method: 'get',
-            headers: {
-              'User-Agent': 'Chrome',
-            },
-            httpsAgent: agent,
-          })
-
-          console.log('1st way =>', responsePromise)
-          const respData = responsePromise.data
-          if (respData.graphql !== undefined) {
-            return res.status(200).json({
-              data: responsePromise.data,
-              status: 'success',
-            })
-          } else {
-            return res.status(200).json({
-              data: {},
-              status: 'error',
-            })
-          }
+          return null
         }
       }
     })
     .catch(function (error) {
-      console.log(error)
-      return res.status(200).json({
-        data: {},
-        status: 'error',
-      })
+      return null
     })
+}
 
+async function getUserInfo(req, res) {
+  const { username } = req.query
+  if (username === '') {
+    throw 'No username given'
+  }
+  const totalProxyCnt = 6
+  let failedProxyCnt = 0
+
+  //1st way
+  firstWay(username, '138.68.76.104', 8058, 'admin:p8FNPlwEEh0R', (result) => {
+    if (result) {
+      return res.status(200).json({
+        data: result,
+        status: 'success',
+      })
+    } else {
+      failedProxyCnt++
+      if (failedProxyCnt === totalProxyCnt) {
+        return res.status(200).json({
+          data: {},
+          status: 'success',
+          message: 'All proxies failed',
+        })
+      }
+    }
+  })
+
+  //2nd way
+  secondWay(
+    username,
+    's2.airproxy.io',
+    31005,
+    {
+      username: 'sanjananb',
+      password: 'sanjananb',
+    },
+    (result) => {
+      if (result) {
+        return res.status(200).json({
+          data: result,
+          status: 'success',
+        })
+      } else {
+        failedProxyCnt++
+        if (failedProxyCnt === totalProxyCnt) {
+          return res.status(200).json({
+            data: {},
+            status: 'success',
+            message: 'All proxies failed',
+          })
+        }
+      }
+    },
+  )
+
+  //3rd way
+  secondWay(
+    username,
+    's2.airproxy.io',
+    30708,
+    {
+      username: 'sanjananb',
+      password: 'sanjananb',
+    },
+    (result) => {
+      if (result) {
+        return res.status(200).json({
+          data: result,
+          status: 'success',
+        })
+      } else {
+        failedProxyCnt++
+        if (failedProxyCnt === totalProxyCnt) {
+          return res.status(200).json({
+            data: {},
+            status: 'success',
+            message: 'All proxies failed',
+          })
+        }
+      }
+    },
+  )
+
+  //4th way
+  secondWay(
+    username,
+    's1.airproxy.io',
+    10902,
+    {
+      username: 'sanjananb',
+      password: 'sanjananb',
+    },
+    (result) => {
+      if (result) {
+        return res.status(200).json({
+          data: result,
+          status: 'success',
+        })
+      } else {
+        failedProxyCnt++
+        if (failedProxyCnt === totalProxyCnt) {
+          return res.status(200).json({
+            data: {},
+            status: 'success',
+            message: 'All proxies failed',
+          })
+        }
+      }
+    },
+  )
+
+  //5th way
+  secondWay(
+    username,
+    's1.airproxy.io',
+    10310,
+    {
+      username: 'sanjananb',
+      password: 'sanjananb',
+    },
+    (result) => {
+      if (result) {
+        return res.status(200).json({
+          data: result,
+          status: 'success',
+        })
+      } else {
+        failedProxyCnt++
+        if (failedProxyCnt === totalProxyCnt) {
+          return res.status(200).json({
+            data: {},
+            status: 'success',
+            message: 'All proxies failed',
+          })
+        }
+      }
+    },
+  )
+
+  //6th way
+  secondWay(
+    username,
+    'proxy.froxy.com',
+    9000,
+    {
+      username: 'BBis8yzzuum8tR9n',
+      password: 'mobile;;;;',
+    },
+    (result) => {
+      if (result) {
+        return res.status(200).json({
+          data: result,
+          status: 'success',
+        })
+      } else {
+        failedProxyCnt++
+        if (failedProxyCnt === totalProxyCnt) {
+          return res.status(200).json({
+            data: {},
+            status: 'success',
+            message: 'All proxies failed',
+          })
+        }
+      }
+    },
+  )
   // data: {
   // 	graphql: {},
   // 	logging_page_id: 'profilePage_20269764',
